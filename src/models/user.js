2b401154f99e886581b458770296c8b4d3e6a750
Tanner Brunscheon <tannerbrunscheon@gmail.com>
2019-02-01 16:14:01 -0600

    Fixed up models

--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,11 @@
-export default class User{
-    constructor(userId){
+class User{
+    constructor(id, googleToken, isActive, dateToRemove){
 
-        this.userId = userId;
-
+        this.id = id; //number
+        this.googleToken = googleToken; //string
+        this.isActive = isActive; //bool
+        this.dateToRemove = dateToRemove; //DateTime
     }
-
-
-
 }
 
-
+export default User;
