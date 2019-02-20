5c5b38abfefce018334c016603672c196c2ec20d
Diego Castaneda <diego-castaneda@uiowa.edu>
2019-02-20 10:43:21 -0600

    Add Joi validation

--- src/handlers/cost-code.js
+++ src/handlers/cost-code.js
@@ -1,22 +1,37 @@
 import Boomify from 'boom';
+import CostCode from '../models/costcode.js';
+
+const Joi = require('joi');
+
+const schema = Joi.object().keys({
+    code: Joi.number().integer().required(),
+    description: Joi.string().min(5).max(140).required()
+});
 
 const functions = Object.create({});
 
 functions.addCostCode = async (request, h) => {
 
-    const code = request.payload.code;
-    const description = request.payload.description;
+    // Validate payload against required schema for a cost-code
+    const validate = Joi.validate(request.payload, schema);
+
+    if ( !(validate.error === null) ) {
+        return Boomify.badRequest('Payload does not match the required schema for a cost-code.');
+    }
+
+    const payLoad = request.payload;
+    const costCode = new CostCode(payLoad.code, payLoad.code, payLoad.description);
 
     const server = require('../server.js');
 
-    // Create new cost code document. If code already exists then update.
+    // Create new cost-code document. If cost-code already exists then update.
     try {
-        await server.db.collection('cost-codes').doc(String(code)).set({
-            code,
-            description
+        await server.db.collection('cost-codes').doc(String(costCode.code)).set({
+            code: costCode.code,
+            description: costCode.description
         });
 
-        return 'Successfully added cost code'; // return 200
+        return h.response(costCode).code(201); // return created status code
     }
     catch (err) {
         return new Boomify(err);
@@ -28,7 +43,7 @@ functions.getCostCodes = async (request, h) => {
     const server = require('../server.js');
 
     try {
-        const snapshot = await server.db.collection('cost-codes').get();
+        const snapshot = await server.db.collection('cost-codes').get(); // All documents in cost-codes collection
         if (snapshot.empty) {
             return {};
         }
