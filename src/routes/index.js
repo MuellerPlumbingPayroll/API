596932f4e512e4a68387ef3ef57b0bebf87d96b8
Diego Castaneda <diego-castaneda@uiowa.edu>
2019-02-28 23:16:44 -0600

    Add user routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import jobs from './jobs';
 import costCodes from './cost-codes';
+import users from './users';
 
 let routes = [];
 
 routes = routes.concat(jobs);
 routes = routes.concat(costCodes);
+routes = routes.concat(users);
 
 export default routes;
