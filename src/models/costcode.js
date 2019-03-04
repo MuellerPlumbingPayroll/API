5b4c0bb8d30b97f0cf1e77a562dc356118bf1863
Diego Castaneda <diego-castaneda@uiowa.edu>
2019-03-04 10:52:25 -0600

    Add code length constraint

--- src/models/costcode.js
+++ src/models/costcode.js
@@ -1,9 +1,13 @@
 const Joi = require('joi');
 
+const codeMaxLength = 8; // Most cost-codes are len 6, so 8 gives some flexibility
+const descriptionMinLength = 5;
+const descriptionMaxLength = 140;
+
 const costCodeSchema = Joi.object().keys({
-    code: Joi.string().required(),
+    code: Joi.string().required().max(codeMaxLength),
     codeGroup: Joi.string().required(),
-    description: Joi.string().min(5).max(140).required()
+    description: Joi.string().min(descriptionMinLength).max(descriptionMaxLength).required()
 });
 
 export default costCodeSchema;
