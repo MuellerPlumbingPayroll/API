f47128df14090a941e452454697827e13a41365d
Diego Castaneda <diego-castaneda@uiowa.edu>
2019-03-06 10:05:08 -0600

    Authenticate email

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import jobs from './jobs';
 import costCodes from './cost-codes';
 import entries from './entries';
 import users from './users';
+import auth from './authentications';
 
 
 let routes = [];
@@ -10,6 +11,7 @@ routes = routes.concat(jobs);
 routes = routes.concat(costCodes);
 routes = routes.concat(entries);
 routes = routes.concat(users);
+routes = routes.concat(auth);
 
 
 export default routes;
