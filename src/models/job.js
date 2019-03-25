768ce23e86a139c53bc117e847a04ba7457e2666
Diego Castaneda <diego-castaneda@uiowa.edu>
2019-03-25 09:27:15 -0500

    Include job number.

--- src/models/job.js
+++ src/models/job.js
@@ -1,7 +1,9 @@
 const Joi = require('joi');
 
+const maxJobNumLength = 9;
 const jobSchema = Joi.object().keys({
     id: Joi.string().allow(null).required(),
+    jobNumber: Joi.string().regex(/^\d+-\d+$/).max(maxJobNumLength).required(),
     clientName: Joi.string().required(),
     address: Joi.string().min(5).max(140).required(),
     isActive: Joi.boolean().required()
