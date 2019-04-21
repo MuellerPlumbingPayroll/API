739d85eef54f983edab77e01047e3bc58695a7ce
Diego Castaneda <diego-castaneda@uiowa.edu>
2019-04-21 16:59:02 -0500

    remove regex on job number

--- src/models/job.js
+++ src/models/job.js
@@ -1,9 +1,8 @@
 const Joi = require('joi');
 
-const maxJobNumLength = 9;
 const jobSchema = Joi.object().keys({
     id: Joi.string().allow(null).required(),
-    jobNumber: Joi.string().regex(/^\d+-\d+$/).max(maxJobNumLength).required(),
+    jobNumber: Joi.string().required(),
     clientName: Joi.string().required(),
     address: Joi.string().min(5).max(140).required(),
     isActive: Joi.boolean().required()
